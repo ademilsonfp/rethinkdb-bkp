f63f43823da3a45f65da5c8e651bee1d2a2477f0
Ademilson <eai@ademilson.udi.br>
2019-08-12 02:45:39 +0000

    choosing database test

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const inquirer = require('inquirer');
 const archiver = require('archiver');
 
 var db;
+var dbName;
 
 async function main() {
   try {
     await connect();
+    await chooseDb();
   } catch (error) {
     console.error(error);
   }
 }
 
 async function connect() {
-  const feedback = ora('Connecting to local RethinkDB');
+  const feedback = ora('Connecting to local RethinkDB...').start();
 
   try {
     db = await r.connect();
@@ -27,7 +29,17 @@ async function connect() {
 }
 
 async function chooseDb() {
+  const feedback = ora('Choosing database...').start();
 
+  try {
+    let names = await r.dbList().run(db);
+    let choosen = await inquirer.prompt({ choices: names });
+
+    console.log(chosen);
+  } catch (error){
+    ora.fail(`Can not use database ${dbName}`);
+    throw error;
+  }
 }
 
 main();
