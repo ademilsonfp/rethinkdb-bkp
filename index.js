352644a9021e6a95a385a8168b3c182a4b8d57c8
Ademilson <eai@ademilson.udi.br>
2019-08-12 04:25:04 +0000

    implementing the rest of inputs

--- index.js
+++ index.js
@@ -2,15 +2,22 @@
 const ora = require('ora');
 const r = require('rethinkdb');
 const inquirer = require('inquirer');
+const luxon = require('luxon');
+const cliProgress = require('cli-progress');
 const archiver = require('archiver');
-
-var db;
-var dbName;
 
 async function main() {
   try {
-    await connect();
-    await chooseDb();
+    let dbConn = await connect();
+    let dbNames = await chooseDbs(dbConn);
+    let dbTables = await getTables(dbConn, dbNames);
+    let docsCount = await countDocs(dbConn, dbTables);
+
+    let timestamp = luxon.DateTime.local().toFormat('yyyyMMddHHmmss');
+    let destPath = await chooseDest(`/tmp/db-${timestamp}Z.zip`);
+
+    // await dumpDocs(dbConn, dbTables, docsCount, destPath);
+    await dbConn.close();
   } catch (error) {
     console.error(error);
   }
@@ -18,28 +25,101 @@ async function main() {
 
 async function connect() {
   const feedback = ora('Connecting to local RethinkDB...').start();
+  var dbConn;
 
   try {
-    db = await r.connect();
+    dbConn = await r.connect();
     feedback.succeed('Connected to local RethinkDB');
   } catch (error) {
     feedback.fail('Can not connect to local RethinkDB');
     throw error;
   }
+
+  return dbConn;
 }
 
-async function chooseDb() {
-  const feedback = ora('Choosing database...').start();
+async function chooseDbs(dbConn) {
+  const feedback = ora('Listing databases...').start();
+  var dbNames;
 
   try {
-    let names = await r.dbList().run(db);
-    let choosen = await inquirer.prompt({ choices: names });
+    let choices = await r.dbList().run(dbConn);
+    feedback.stop();
 
-    console.log(chosen);
-  } catch (error){
-    ora.fail(`Can not use database ${dbName}`);
+    ({ dbNames } = await inquirer.prompt({
+      choices,
+      name: 'dbNames',
+      type: 'checkbox',
+      message: 'Select one or more databases'
+    }));
+  } catch (error) {
+    if (feedback.isSpinning) {
+      feedback.fail('Can not list databases');
+    }
+
     throw error;
   }
+
+  return dbNames;
+}
+
+async function getTables(dbConn, dbNames) {
+  const feedback = ora('Finding tables...').start();
+  const dbTables = {};
+
+  try {
+    let count = 0;
+
+    for (let i = 0, name; i < dbNames.length; i++) {
+      name = dbNames[i];
+      dbTables[name] = await r.db(name).tableList().run(dbConn);
+      count += dbTables[name].length;
+    }
+
+    feedback.succeed(`${count} tables found`);
+  } catch (error) {
+    feedback.fail('Can not find tables');
+  }
+
+  return dbTables;
+}
+
+async function countDocs(dbConn, dbTables) {
+  const feedback = ora('Counting documents...').start();
+  var count = 0;
+
+  try {
+    let tables;
+
+    for (let dbName in dbTables) {
+      tables = dbTables[dbName];
+
+      for (let i = 0, name; i < tables.length; i++) {
+        name = tables[i];
+        count += await r.db(dbName).table(name).count().run(dbConn);
+      }
+    }
+
+    feedback.succeed(`${count} documents found`);
+  } catch (error) {
+    feedback.fail('Can not count documents');
+  }
+
+  return count;
+}
+
+async function chooseDest(defaul) {
+  const { destPath } = await inquirer.prompt({
+    default: defaul,
+    name: 'destPath',
+    message: 'Select destination'
+  });
+
+  return destPath;
+}
+
+async function dumpDocs(destPath, dbConn, dbTables, docsCount) {
+
 }
 
 main();
